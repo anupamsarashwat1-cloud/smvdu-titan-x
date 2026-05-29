097e70fbde1ccf87ce4827c65712523fc5f0ab8f
Anupam Sarashwat <23bec014@smvdu.ac.in>
2026-05-29 08:09:04 +0530

    Update open_gds.tcl to prefix commands with magic1 to ensure proper window context and avoid titlebar widget crash

--- asic/ASIC through Open Source tools/docs/open_gds.tcl
+++ asic/ASIC through Open Source tools/docs/open_gds.tcl
@@ -1,7 +1,7 @@
 gds read "/home/anupam-sarashwat/Documents/antigravity/cool-hawking/asic/ASIC through Open Source tools/delivery/titan_x_top.gds"
 after 1000 {
-    load titan_x_top
-    select top cell
-    expand
-    view
+    magic1 load titan_x_top
+    magic1 select top cell
+    magic1 expand
+    magic1 view
 }

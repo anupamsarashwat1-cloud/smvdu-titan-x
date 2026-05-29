c3f5023b139d3ff3d00a38df1ad2711772523f2d
Anupam Sarashwat <23bec014@smvdu.ac.in>
2026-05-29 07:37:17 +0530

    Update open_gds.tcl with a 1-second delay for load and expand to prevent startup GUI race condition

--- asic/ASIC through Open Source tools/docs/open_gds.tcl
+++ asic/ASIC through Open Source tools/docs/open_gds.tcl
@@ -1,5 +1,7 @@
 gds read "/home/anupam-sarashwat/Documents/antigravity/cool-hawking/asic/ASIC through Open Source tools/delivery/titan_x_top.gds"
-load titan_x_top
-select top cell
-expand
-view
+after 1000 {
+    load titan_x_top
+    select top cell
+    expand
+    view
+}
